26d87dcf137e2ff64f9b6a462ce9de41ada8f154
CarlosVictorAR <carlosvgitprofissional@gmail.com>
2025-11-03 17:14:10 -0300

    new: added files JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let arrayOfItens = [];
+class Item {
+    constructor (id, nome, categoria, quantidade, preco, dataEntrada){
+        this.id = id;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.dataEntrada = dataEntrada;
+    }
+}
+
